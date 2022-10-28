c67d28c8cccbd43824ff9cccfad48711174babde
andrejnovkovikj <116837194+andrejnovkovikj@users.noreply.github.com>
2022-10-28 10:49:47 +0200

    Update BalloonServiceImpl.java

--- src/main/java/mk/finki/ukim/mk/lab/service/impl/BalloonServiceImpl.java
+++ src/main/java/mk/finki/ukim/mk/lab/service/impl/BalloonServiceImpl.java
@@ -1,6 +1,3 @@
-package mk.finki.ukim.mk.lab.service.impl;
-
-
 import mk.finki.ukim.mk.lab.model.Balloon;
 import mk.finki.ukim.mk.lab.repository.BalloonRepository;
 import mk.finki.ukim.mk.lab.service.BalloonService;
@@ -13,18 +10,30 @@ public class BalloonServiceImpl implements BalloonService {
     private final BalloonRepository balloonRepository;
 
     public BalloonServiceImpl(BalloonRepository balloonRepository) {
-
         this.balloonRepository = balloonRepository;
     }
 
     @Override
     public List<Balloon> listAll() {
-
         return balloonRepository.findAllBalloons();
     }
 
     @Override
     public List<Balloon> searchByNameOrDescription(String text) {
         return balloonRepository.findAllByNameOrDescription(text);
+    }
+    public int getListSize(){
+        return balloonRepository.size();
+    }
+    public Balloon getColor(String name){
+        if (name==null || name.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+        Balloon b = new Balloon(name);
+        balloonRepository.chooseColor(b);
+        return b;
+    }
+    public Balloon getLastElement(){
+        return balloonRepository.chooseLastElement();
     }
 }
